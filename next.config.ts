ee52f52b4e9022960e9aaa8791321225a1e7c4f0
Supraja <suprajapalagiri96@gmail.com>
2026-02-02 14:08:16 +0530

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  /* config options here */
 };
 
 export default nextConfig;
