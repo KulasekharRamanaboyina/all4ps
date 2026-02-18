328515e8d24b70076e9a7268fae11e1b1c5e377f
Supraja <suprajapalagiri96@gmail.com>
2026-02-18 17:10:44 +0530

    modified next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,31 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {};
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.all4ps.co",
-          },
-        ],
-        destination: "https://all4ps.co/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   trailingSlash: false,
+
+//   async redirects() {
+//     return [
+//       {
+//         source: "/:path*",
+//         has: [
+//           {
+//             type: "host",
+//             value: "www.all4ps.co",
+//           },
+//         ],
+//         destination: "https://all4ps.co/:path*",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
